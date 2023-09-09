d83d6d30803ffafcec4cfe6183e84a60e7c1ea04
sedew810225 <sedew810225@gmail.com>
2023-09-09 20:22:17 +0900

    LeetCode 784 LetterCasePermutation Python
    test set update

--- LeetCode_784_LetterCasePermutation/test_main.py
+++ LeetCode_784_LetterCasePermutation/test_main.py
@@ -4,8 +4,8 @@ from main import Solution
 class TestSolution(TestCase):
     def test_letter_case_permutation(self):
         testcase = Solution()
-        self.assertEqual( ["a1b2","a1B2","A1b2","A1B2"] , testcase.letterCasePermutation("a1b2"))
+        self.assertEqual( set(["a1b2","a1B2","A1b2","A1B2"]) , set(testcase.letterCasePermutation("a1b2")))
 
     def test2_letter_case_permutation(self):
         testcase = Solution()
-        self.assertEqual( ["c","C"] , testcase.letterCasePermutation("C"))
+        self.assertEqual( set(["c","C"]) , set(testcase.letterCasePermutation("C")))
