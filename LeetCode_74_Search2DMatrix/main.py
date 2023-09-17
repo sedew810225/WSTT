f0bc734b0c2ffd4c2a210bfb2289bb4add492522
sedew810225 <sedew810225@gmail.com>
2023-09-17 19:37:23 +0900

    LeetCode 74 Search2DMatrix Python 2

--- LeetCode_74_Search2DMatrix/main.py
+++ LeetCode_74_Search2DMatrix/main.py
@@ -17,12 +17,45 @@ class Solution:
         else:
             return self.binSearch( matrix, target, startnum, mid-1, colcnt)
 
+    def binSearch2_col(self, matrixcol: List[int], target: int, startnum: int, endnum: int) -> bool:
+
+        if endnum < startnum:
+            return False
+
+        mid_col = int((startnum+endnum)/2)
+
+        if matrixcol[mid_col] == target:
+            return True
+        elif matrixcol[mid_col] < target:
+            startnum = mid_col+1
+        elif matrixcol[mid_col] > target:
+            endnum = mid_col-1
+
+        return self.binSearch2_col(matrixcol, target, startnum, endnum)
+
+    def binSearch2_row(self, matrix: List[List[int]], target: int, startnum: int, endnum: int) -> bool:
+
+        if endnum < startnum:
+            return False
+
+        mid_row = int((startnum+endnum)/2)
+        mid_len = len(matrix[mid_row])-1
+
+        if matrix[mid_row][0] <= target <= matrix[mid_row][mid_len]:
+            return self.binSearch2_col(matrix[mid_row], target, 0, mid_len)
+        elif target < matrix[mid_row][0]:
+            endnum = mid_row -1
+        elif target > matrix[mid_row][len(matrix[mid_row])-1]:
+            startnum = mid_row+1
+
+        return self.binSearch2_row(matrix, target, startnum, endnum)
+
+
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
 
-        rowcnt = len(matrix)
-        colcnt = len(matrix[0])
-        endindex = rowcnt*colcnt-1
+        #rowcnt = len(matrix)
+        #colcnt = len(matrix[0])
+        #endindex = rowcnt*colcnt-1
+        #return self.binSearch(matrix, target, 0, endindex, colcnt)
 
-        return self.binSearch( matrix, target, 0, endindex, colcnt)
-
-
+        return self.binSearch2_row(matrix, target, 0, len(matrix)-1)
--- LeetCode_74_Search2DMatrix/test_main.py
+++ LeetCode_74_Search2DMatrix/test_main.py
@@ -12,3 +12,19 @@ class TestSolution(TestCase):
     def test3_search_matrix(self):
         testcase = Solution()
         self.assertEqual(False, testcase.searchMatrix([[1,1]], 0))
+
+    def test4_search_matrix(self):
+        testcase = Solution()
+        self.assertEqual(False, testcase.searchMatrix([[1]], 2))
+
+    def test5_search_matrix(self):
+        testcase = Solution()
+        self.assertEqual(True, testcase.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,50]], 11))
+
+    def test6_search_matrix(self):
+        testcase = Solution()
+        self.assertEqual(True, testcase.searchMatrix([[1],[3]], 3))
+
+    def test7_search_matrix(self):
+        testcase = Solution()
+        self.assertEqual(True, testcase.searchMatrix([[1],[3],[5]], 3))
