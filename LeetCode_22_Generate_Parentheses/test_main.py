cfb6651cd7483e57e06182b89510c0c43e8214f2
sedew810225 <sedew810225@gmail.com>
2023-11-08 22:51:45 +0900

    Revert "LeetCode 22 Generate Parentheses Python"
    This reverts commit 65e07795b138645aa310429aaa1ee6e253db4503.

--- LeetCode_22_Generate_Parentheses/test_main.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-from main import Solution
-
-class TestSolution(TestCase):
-    def test_generate_parenthesis(self):
-        testcase = Solution()
-        self.assertEqual(["((()))","(()())","(())()","()(())","()()()"], testcase.generateParenthesis(3))
--- LeetCode_739_Daily_Temperatures/test_main.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-from main import Solution
-
-class TestSolution(TestCase):
-    def test_daily_temperatures(self):
-        testcase = Solution()
-        self.assertEqual([1,1,4,2,1,1,0,0], testcase.dailyTemperatures([73,74,75,71,69,72,76,73]))
