778c13c4193a6ccfa624c98dd99c70e52ef95c1d
sedew810225 <sedew810225@gmail.com>
2023-11-08 22:59:06 +0900

    LeetCode 22 Generate Parentheses Python

---
+++ LeetCode_22_Generate_Parentheses/main.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        result = []
+
+        def addMore(aStack: str, lcnt: int, rcnt: int, n: int):
+
+            if n == lcnt == rcnt:
+                result.append(aStack)
+                return
+
+            if lcnt < n or lcnt == rcnt:
+                addMore(aStack + '(', lcnt + 1, rcnt, n)
+
+            if rcnt < n and lcnt != rcnt and lcnt > rcnt:
+                addMore(aStack + ')', lcnt, rcnt + 1, n)
+
+        addMore("(", 1, 0, n)
+        return result
